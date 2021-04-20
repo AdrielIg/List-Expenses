521f6161b79269977a1d08b1f0494f3a2f57e9f1
AdrielIg <adrielignaciogomez@gmail.com>
2021-04-20 17:25:52 -0300

    Added functional charts for every month

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,14 @@
+import "./Chart.css"
+import ChartBar from "./ChartBar"
+
+const Char = (props) => {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
+    const totalMaximum = Math.max(...dataPointValues)
+
+    return (<div className="chart">
+        {props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} key={dataPoint.label} />)}
+
+    </div>)
+}
+
+export default Char
