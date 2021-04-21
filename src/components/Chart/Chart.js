e8a2b027a6d37a8bd4c11ff8cfe0e3bf9af08918
AdrielIg <adrielignaciogomez@gmail.com>
2021-04-20 22:55:02 -0300

    Added show chart button

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,14 +1,36 @@
+import { useState } from "react"
 import "./Chart.css"
 import ChartBar from "./ChartBar"
 
 const Char = (props) => {
+    const [isShowChart, setIsShowChart] = useState(false)
+
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
     const totalMaximum = Math.max(...dataPointValues)
 
-    return (<div className="chart">
-        {props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} key={dataPoint.label} />)}
 
-    </div>)
+
+    const showChartHandler = () => {
+        setIsShowChart(!isShowChart)
+    }
+
+    if (isShowChart) {
+        return (
+            <div>
+                <button className="chart-button" onClick={showChartHandler} >Close Chart</button>
+                <div className="chart">
+
+                    {props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} key={dataPoint.label} />)}
+
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <button className="chart-button" onClick={showChartHandler} > Show Chart</button>
+    )
+
 }
 
 export default Char
